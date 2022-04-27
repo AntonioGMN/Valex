3a3d25d0b62be85af621885e01ad56d60ef76c31
Liaess <yann.melo@gmail.com>
2022-04-27 15:04:58 -0300

    Preparando revisão de código

--- src/Routers/patmentsRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { payment } from "../Controllers/paymentController.js";
-import validateSchema from "../Middlerware/validateSchema.js";
-import rechargeSchema from "../Schema/rechargeSchema.js";
-
-const paymentRouter = Router();
-paymentRouter.post("/payment", payment);
-
-export default paymentRouter;
